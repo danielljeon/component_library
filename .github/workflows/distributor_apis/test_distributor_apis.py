a3cd3c1f8d4aea3e7955a1442c38a3868351c181
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-07-27 16:13:51 -0400

    Add pytest and coverage for workflows (#1)
    * Implement pytest
    
    * Minor format fix
    
    * Fix broken workflow needs
    
    * Fix borkd file paths
    
    * Update workflow for coverage
    
    * Test forced failure of workflow
    
    * Remove prev test and add docstring

---
+++ .github/workflows/distributor_apis/test_distributor_apis.py
@@ -0,0 +1,30 @@
+"""pytest and pytest-cov functions for distributor_apis."""
+
+from distributor_apis_digikey import DigiKeyAPI
+from distributor_apis_mouser import MouserAPI
+
+
+def test_mouser_part_number_search():
+    # Mouser part number search.
+    mouser_part_number = ""
+    mouser_result = MouserAPI.search_part(part_number=mouser_part_number)
+    assert isinstance(
+        mouser_result, list
+    ), "Mouser API part number search failed"
+
+
+def test_digikey_part_number_search():
+    # DigiKey OAuth bearer token request.
+    token, lifetime = DigiKeyAPI.get_new_bearer_token()
+    assert (
+        isinstance(lifetime, int) and lifetime > 0
+    ), "DigiKey API bearer token request failed"
+
+    # DigiKey part number search.
+    digikey_part_number = "qwerty123456"
+    digikey_result = DigiKeyAPI.search_part(
+        bearer_token=token, part_number=digikey_part_number
+    )
+    assert isinstance(
+        digikey_result, list
+    ), "DigiKey API part number search failed"
